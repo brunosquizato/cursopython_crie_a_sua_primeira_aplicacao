d5a757465835072aa26cc6c05aebda10bbe0d0f7
Bruno Eugenio Squizato <bruno_squizato@hotmail.com>
2024-10-04 14:57:41 -0300

    first commit

---
+++ exercicios_aula01.py
@@ -0,0 +1,19 @@
+print('Exercicio 1:')
+# imprima a frase: Python na Escola de Programação Alura
+print('Python na Escola de Programação Alura')
+
+print('\nExercicio 2:')
+# Imprima a frase: 'Meu nome é {nome} e tenho {idade} anos' em que nome e idade precisam ser valores armazenados em variaveis
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+print(f'Meu nome é {nome} e tenho {idade} anos')
+
+print('\nExercicio 3:')
+#Imprima a palavra: ‘ALURA’ de modo que cada letra fique em uma linha
+print('A\nL\nU\nR\nA')
+
+print('\nExercicio 4:')
+#Imprima a frase: O valor arredondado de pi é: {pi_arredondado} em que o valor de pi precisa ser armazenado em uma variável e arredondado para apenas duas casas decimais.
+pi = 3.14159
+pi_arredondado = round(pi,2)
+print(f'O valor arredondado de pi é: {pi_arredondado}')
